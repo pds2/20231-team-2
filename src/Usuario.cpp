513f683490634ca40098a8c18dc4da0189193a9e
Erik Neves <erikrrn04@gmail.com>
2023-06-10 21:10:05 -0300

    Update Usuario.cpp

--- src/Usuario.cpp
+++ src/Usuario.cpp
@@ -2,17 +2,21 @@
 
 #include "../include/Usuario.hpp"
 
-Usuario::Usuario(std::string nome, std::string login, std::string senha): _nome(nome), _login(login), _senha(senha) {}
+Usuario::Usuario(std::string nome, std::string login, std::string senha)
+    : _nome(nome), _login(login), _senha(senha) 
+{   }
 
-std::string Usuario::GetLogin(){
+std::string Usuario::GetLogin()
+{
     return _login;
 };
 
-std::string Usuario::GetSenha(){
+std::string Usuario::GetSenha()
+{
     return _senha;
 };
 
-
-std::string Usuario::GetNome(){
+std::string Usuario::GetNome()
+{
     return _nome;
 };
