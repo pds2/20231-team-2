be191344ebf1a72b5cb7c9229104c27517c865a1
Erik Neves <erikrrn04@gmail.com>
2023-06-10 21:39:29 -0300

    Restaurante reference to item

--- include/Restaurante.hpp
+++ include/Restaurante.hpp
@@ -4,8 +4,10 @@
 #define RESTAURANTE_HPP
 
 #include <string>
+#include <vector>
 
 #include "Usuario.hpp"
+#include "Item.hpp"
 
 /*
  *  Essa classe é responsável por armazenar e inicializar as informações básicas
@@ -14,16 +16,33 @@
 class Restaurante : public Usuario {
     private:
         std::string _CNPJ;
+        std::vector<Item*> _itens;
     public:
-        /*
+        /**
          * @brief Instância de um novo restaurante.
          */
         Restaurante(std::string nome, std::string login, std::string senha,std::string CNPJ);
 
-        /*
+        /**
          * @brief Retorna o CNPJ de um restaurante.
          */
-       std::string GetCNPJ();
+        std::string GetCNPJ();
+
+        /**
+         * @brief Adiciona um item a esse restaurante.
+        */
+        void AdicionarItem(Item* item);
+
+        /**
+         * @brief Remove um item desse restaurante.
+        */
+        void RemoverItem(Item* item);
+
+        /**
+         * @brief Acesso aos itens desse restaurante;
+         * @returns Um vector contendo ponteiros pros itens.
+        */
+        std::vector<Item*> GetItens();
 };
 
 #endif
--- src/Restaurante.cpp
+++ src/Restaurante.cpp
@@ -2,8 +2,39 @@
 
 #include "../include/Restaurante.hpp"
 
-Restaurante::Restaurante(std::string nome, std::string login, std::string senha, std::string CNPJ):Usuario(nome, login, senha), _CNPJ(CNPJ){}
+Restaurante::Restaurante(std::string nome, std::string login, std::string senha, std::string CNPJ)
+    :Usuario(nome, login, senha), _CNPJ(CNPJ)
+{}
 
-std::string Restaurante::GetCNPJ(){
+std::string Restaurante::GetCNPJ()
+{
     return _CNPJ;
 };
+
+void Restaurante::AdicionarItem(Item* novoItem)
+{
+    for(Item* item : _itens)
+    {
+        if (item->GetId() == novoItem->GetId())
+            return;
+    }
+
+    _itens.push_back(novoItem);
+}
+
+void Restaurante::RemoverItem(Item* alvo)
+{
+    for (auto it = _itens.begin(); it != _itens.end(); ++it)
+    {
+        if ((*it)->GetId() == alvo->GetId())
+        {
+            _itens.erase(it);
+            break;
+        }
+    }
+}
+
+std::vector<Item*> Restaurante::GetItens()
+{
+    return _itens;
+}
