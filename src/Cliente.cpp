6c9ec57b29669bf2b60bc5b3789539509651d80b
Erik Neves <erikrrn04@gmail.com>
2023-06-19 20:45:59 -0300

    add tipo de usuário

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -2,7 +2,9 @@
 
 #include "../include/Cliente.hpp"
 
-Cliente::Cliente(std::string nome, std::string login, std::string senha, std::string CPF):Usuario(nome, login, senha), _CPF(CPF){}
+Cliente::Cliente(std::string nome, std::string login, std::string senha, std::string CPF)
+    :Usuario(nome, login, senha, TipoUsuario::CLIENTE), _CPF(CPF)
+{}
 
 std::string Cliente::GetCPF(){
     return _CPF;
