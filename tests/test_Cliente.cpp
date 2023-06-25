97db62db0850102388fea76bfe7f7a53d103d34d
hellolima <gabrielladelimadev@gmail.com>
2023-06-25 12:05:54 -0300

    feature: testes em cliente

---
+++ tests/test_Cliente.cpp
@@ -0,0 +1,33 @@
+#include "../third_party/doctest.h"
+#include "../include/Cliente.hpp"
+
+TEST_CASE("Teste GetCPF") {
+    Cliente cliente("nome", "login", "senha", "123456789");
+    CHECK(cliente.GetCPF() == "123456789");
+}
+
+TEST_CASE("Teste GetCarteira") {
+    Cliente cliente("nome", "login", "senha", "123456789");
+    Carteira* carteira = new Carteira(1);
+    cliente.SetCarteira(carteira);
+    CHECK(cliente.GetCarteira()->GetSaldo() == 0.0);
+    delete carteira;
+}
+
+TEST_CASE("Teste AdicionarRemoverCarrinho") {
+    Cliente cliente("Nome", "login", "senha", "123456789");
+    Carrinho* carrinho1 = new Carrinho(1);
+    Carrinho* carrinho2 = new Carrinho(1);
+    cliente.AdicionarCarrinho(carrinho1);
+    cliente.AdicionarCarrinho(carrinho2);
+
+    std::vector<Carrinho*> carrinhos = cliente.GetCarrinhos();
+    CHECK(carrinhos.size() == 2);
+
+    cliente.RemoverCarrinho(carrinho1);
+    carrinhos = cliente.GetCarrinhos();
+    CHECK(carrinhos.size() == 1);
+
+    delete carrinho1;
+    delete carrinho2;
+}
