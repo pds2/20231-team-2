3a823023c6be31098e9ecbbb056f94bc1a8cf1c3
Erik Neves <erikrrn04@gmail.com>
2023-07-08 17:44:34 -0300

    login automatico ao cadastrar usuário

--- src/Servicos/CadastroServico.cpp
+++ src/Servicos/CadastroServico.cpp
@@ -1,24 +1,23 @@
-#include "../../include/Servicos/CadastroServico.hpp"
+#include "Servicos/CadastroServico.hpp"
 
 #include <string>
 #include <iostream>
 
+#include "cores.hpp"
+#include "Usuario.hpp"
 #include "Cliente.hpp"
+#include "Restaurante.hpp"
 #include "EntidadeBase.hpp"
 #include "Repositories/ClienteRepositorio.hpp"
 #include "Repositories/DatabaseManager.hpp"
 #include "Repositories/RestauranteRepositorio.hpp"
-#include "Restaurante.hpp"
-#include "Usuario.hpp"
-#include "../../third_party/cores.hpp"
-
 
 CadastroServico::CadastroServico(DatabaseManager *dbManager) {
   _clienteRepositorio = dbManager->GetClienteRepositorio();
   _restauranteRepositorio = dbManager->GetRestauranteRepositorio();
 }
 
-void CadastroServico::escolherTipoCadastro(){
+Usuario* CadastroServico::MenuCadastro(){
     int opcao;
     std::cout << CIANO << "Escolha o tipo de cadastro! Pressione" << RESET <<std::endl;
     std::cout << "[1] Cliente" << std::endl;
@@ -29,48 +28,52 @@ void CadastroServico::escolherTipoCadastro(){
     std::string nome, login, senha, cpf, cnpj;
 
     switch (opcao) {
-    case 1:
-      std::cout << "Digite o nome do cliente: ";
-      std:: cin >> nome;
-      std::cout << "Digite o CPF do cliente: ";
-      std::cin >> cpf;
-      std::cout << "Digite o login do cliente: ";
-      std::cin >> login;
-      std::cout << "Digite a senha do cliente: ";
-      std::cin >> senha;
-      cadastrarCliente(nome, cpf, login, senha);
-      break;
-     case 2:
-      std::cout << "Digite o nome do restaurante: ";
-      std:: cin >> nome;
-      std::cout << "Digite o CNPJ do restaurante: ";
-      std::cin >> cnpj;
-      std::cout << "Digite o login do restaurante: ";
-      std::cin >> login;
-      std::cout << "Digite a senha do restaurante: ";
-      std::cin >> senha;
-      cadastrarRestaurante(nome, cnpj, login, senha);
-      break;
+      case 1:
+        std::cout << "Digite o nome do cliente: ";
+        std:: cin >> nome;
+        std::cout << "Digite o CPF do cliente: ";
+        std::cin >> cpf;
+        std::cout << "Digite o login do cliente: ";
+        std::cin >> login;
+        std::cout << "Digite a senha do cliente: ";
+        std::cin >> senha;
+        return CadastrarCliente(nome, cpf, login, senha);
+        break;
+      case 2:
+        std::cout << "Digite o nome do restaurante: ";
+        std:: cin >> nome;
+        std::cout << "Digite o CNPJ do restaurante: ";
+        std::cin >> cnpj;
+        std::cout << "Digite o login do restaurante: ";
+        std::cin >> login;
+        std::cout << "Digite a senha do restaurante: ";
+        std::cin >> senha;
+        return CadastrarRestaurante(nome, cnpj, login, senha);
+        break;
 
-    default:
-      std::cout << VERMELHO << "Opção inválida." << RESET << std::endl;
-      break; 
+      default:
+        std::cout << VERMELHO << "Opção inválida." << RESET << std::endl;
+        break; 
     }
+
+    return nullptr;
 }
 
-void CadastroServico::cadastrarCliente(std::string& nome, std::string& cpf, std::string& login, std::string& senha) {
+Cliente* CadastroServico::CadastrarCliente(std::string& nome, std::string& cpf, std::string& login, std::string& senha) {
   Cliente* novoCliente = new Cliente(nome, login, senha, cpf);
   try {
-    _clienteRepositorio->Inserir(novoCliente);
     Carteira* novaCarteira = new Carteira(novoCliente->GetId());
     novoCliente->SetCarteira(novaCarteira);
+    _clienteRepositorio->Inserir(novoCliente);
     std::cout << VERDE << "Cadastro realizado com sucesso!" << RESET << std::endl;
   } catch (std::exception& ex) {
     std::cout << "Erro ao cadastrar cliente: " << ex.what() << std::endl;
   }
+
+  return novoCliente;
 }
 
-void CadastroServico::cadastrarRestaurante(std::string& nome, std::string& cnpj, std::string& login, std::string& senha) {
+Restaurante* CadastroServico::CadastrarRestaurante(std::string& nome, std::string& cnpj, std::string& login, std::string& senha) {
   Restaurante* novoRestaurante = new Restaurante(nome, login, senha, cnpj);
   try {
     _restauranteRepositorio->Inserir(novoRestaurante);
@@ -78,5 +81,6 @@ void CadastroServico::cadastrarRestaurante(std::string& nome, std::string& cnpj,
   } catch (std::exception& ex) {
     std::cout << "Erro ao cadastrar Restaurante: " << ex.what() << std::endl;
   }
-}
 
+  return novoRestaurante;
+}
