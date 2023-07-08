c3546982519af09438be51a1f38f91b577dfaa3b
Gabriella <gabrielladelimadev@gmail.com>
2023-07-08 18:00:49 -0300

    update: cupom

--- include/Cupom.hpp
+++ include/Cupom.hpp
@@ -1,51 +1,61 @@
+#pragma once
+
 #ifndef CUPOM_HPP
 #define CUPOM_HPP
 
 #include <string>
-
 #include "EntidadeBase.hpp"
 
-/*
- *@brief Essa classe representa um cupom que poderá ser usado
- *para que o cliente tenha desconto (em porcentagem) nas compras.
+/**
+ * @class Cupom
+ * @brief Classe que representa um cupom de desconto.
+ *
+ * Essa classe representa um cupom que pode ser utilizado para fornecer descontos aos clientes em suas compras.
  */
-class Cupom: public EntidadeBase{
-  private:
-  std::string _codigo;
-  double _desconto;
-  bool _valido;
-  //poderá ser definido mais tarde
-  std::string _dataExpiracao;
+class Cupom : public EntidadeBase {
+private:
+    std::string _codigo;
+    double _desconto;
+    bool _valido;
+    //pode ser definido mais tarde
+    std::string _dataExpiracao;
 
-  public:
-  /*
-   *@brief Cria um novo cupom de desconto
-   */
-  Cupom(std::string codigo, double desconto);
+public:
+    /**
+     * @brief Construtor da classe Cupom.
+     * @param codigo O código do cupom.
+     * @param desconto O valor do desconto em porcentagem.
+     */
+    Cupom(std::string codigo, double desconto);
 
-  /*
-   * @brief Retorna o código do cupom de desconto 
-   */
-  std::string GetCodigo();
+    /**
+     * @brief Retorna o código do cupom.
+     * @return O código do cupom.
+     */
+    std::string GetCodigo();
 
-  /*
-   * @brief Retorna o valor do cupom de desconto 
-   */
-  double GetValor();
+    /**
+     * @brief Retorna o valor do desconto do cupom.
+     * @return O valor do desconto em porcentagem.
+     */
+    double GetValor();
 
-  std::string GetDataDeExpiracao();
-  
-  /*
-   * @brief Verifica se o código está dentro da 
-   * data de expiração 
-   */
-  bool EstaValido();
+    /**
+     * @brief Retorna a data de expiração do cupom.
+     * @return A data de expiração do cupom.
+     */
+    std::string GetDataDeExpiracao();
 
-  /**
-   * @brief Seta o status do cupom.
-  */
-  void SetaValido();
+    /**
+     * @brief Verifica se o cupom está válido.
+     * @return true se o cupom estiver válido, false caso contrário.
+     */
+    bool EstaValido();
 
+    /**
+     * @brief Define o status do cupom como válido.
+     */
+    void SetaValido();
 };
 
-#endif
+#endif // CUPOM_HPP
