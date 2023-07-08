d70a6b8e686a5d891d446e9d8e300a5e557de0b5
Gabriella <gabrielladelimadev@gmail.com>
2023-07-08 17:47:11 -0300

    update: usuario, cliente e restaurante

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -4,66 +4,82 @@
 #define CLIENTE_HPP
 
 #include <string>
+#include <vector>
 
 #include "Usuario.hpp"
 #include "Carteira.hpp"
 #include "Carrinho.hpp"
 
-/*
- *  Essa classe é responsável por armazenar e inicializar as informações básicas
- *  de um usuário do tipo cliente.
+/**
+ * @class Cliente
+ * @brief Classe que representa um cliente.
+ *
+ * Essa classe é responsável por armazenar e inicializar as informações básicas
+ * de um usuário do tipo cliente.
  */
 class Cliente : public Usuario {
-    private:
-        std::string _CPF;
-        Carteira* _carteira;
-        std::vector<Carrinho*> _carrinhos;
-    public:
-        /**
-         * @brief Instância de um novo cliente.
-         */
-        Cliente(std::string nome, std::string login, std::string senha, std::string CPF);
+private:
+    std::string _CPF;
+    Carteira* _carteira;
+    std::vector<Carrinho*> _carrinhos;
 
-        /**
-         * @brief Retorna o CPF de um cliente.
-         */
-        std::string GetCPF();
+public:
+    /**
+     * @brief Construtor da classe Cliente.
+     * @param nome O nome do cliente.
+     * @param login O login do cliente.
+     * @param senha A senha do cliente.
+     * @param CPF O CPF do cliente.
+     */
+    Cliente(std::string nome, std::string login, std::string senha, std::string CPF);
 
-        /**
-         * @brief Retorna um ponteiro pra carteira desse cliente.
-         */
-        Carteira* GetCarteira();
+    /**
+     * @brief Retorna o CPF do cliente.
+     * @return O CPF do cliente.
+     */
+    std::string GetCPF();
 
-        /**
-         * @brief Seta o ponteiro pra carteira desse cliente.
-         */
-        void SetCarteira(Carteira* carteira);
+    /**
+     * @brief Retorna um ponteiro para a carteira desse cliente.
+     * @return O ponteiro para a carteira do cliente.
+     */
+    Carteira* GetCarteira();
 
-        /**
-         * @brief Adiciona um carrinho a esse cliente.
-        */
-        void AdicionarCarrinho(Carrinho* carrinho);
+    /**
+     * @brief Define a carteira desse cliente.
+     * @param carteira O ponteiro para a carteira do cliente.
+     */
+    void SetCarteira(Carteira* carteira);
 
-        /**
-         * @brief Remover um carrinho desse cliente.
-        */
-        void RemoverCarrinho(Carrinho* carrinho);
+    /**
+     * @brief Adiciona um carrinho a esse cliente.
+     * @param carrinho O carrinho a ser adicionado.
+     */
+    void AdicionarCarrinho(Carrinho* carrinho);
 
-        /**
-         * @brief Retorna os carrinhos desse cliente.
-        */
-        std::vector<Carrinho*> GetCarrinhos();
+    /**
+     * @brief Remove um carrinho desse cliente.
+     * @param carrinho O carrinho a ser removido.
+     */
+    void RemoverCarrinho(Carrinho* carrinho);
 
-        /**
-         * @brief Substitui o vector de carrinhos desse cliente.
-         * @param carrinhos Novos dados.
-        */
-        void SetCarrinhos(std::vector<Carrinho*> carrinhos);
+    /**
+     * @brief Retorna os carrinhos desse cliente.
+     * @return Um vetor de ponteiros para os carrinhos do cliente.
+     */
+    std::vector<Carrinho*> GetCarrinhos();
 
-        /**
-         * @brief Retorna o tipo de um usário, neste caso, cliente.
-         */
-        TipoUsuario GetTipo() override;
+    /**
+     * @brief Define os carrinhos desse cliente.
+     * @param carrinhos O vetor de ponteiros para os carrinhos do cliente.
+     */
+    void SetCarrinhos(std::vector<Carrinho*> carrinhos);
+
+    /**
+     * @brief Retorna o tipo do usuário, neste caso, cliente.
+     * @return O tipo do usuário.
+     */
+    TipoUsuario GetTipo() override;
 };
 
-#endif
+#endif // CLIENTE_HPP
